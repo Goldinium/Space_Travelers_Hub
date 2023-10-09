acec333f0a63d6bc5f74dcaf0a89ec28fd175cc4
ometman <revomet@gmail.com>
2023-10-09 18:20:40 +0000

    Add planet-icon logo to navbar

--- src/routes/NavLink.js
+++ src/routes/NavLink.js
@@ -1,12 +1,16 @@
 import { Link, Outlet } from 'react-router-dom';
+import Planeticon from '../assets/Planeticon.png';
 
 function NavLink() {
   return (
     <div className="root-container">
       <div className="header">
-        <h1 className="heading">
-          Space Traveler&apos;s Hub
-        </h1>
+        <div className="logo-header">
+          <img src={Planeticon} alt="planet-icon" />
+          <h1 className="heading">
+            Space Traveler&apos;s Hub
+          </h1>
+        </div>
         <nav>
           <ul className="nav-items">
             <li><Link to="home">Rockets</Link></li>
