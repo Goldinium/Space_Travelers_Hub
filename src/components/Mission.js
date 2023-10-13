5a38e658da915514a0541bb92138bd764493f8c5
ometman <revomet@gmail.com>
2023-10-13 19:38:48 +0000

    fix conflict on missions

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { cancelMission, reserveMission } from '../redux/missions/missionsSlice';
@@ -27,24 +26,6 @@ const Mission = ({ mission }) => {
       dispatch(cancelMission(id));
     }
   };
-=======
-// import { useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
-// import { removeMissionByThunk } from '../redux/missions/missionsSlice';
-
-const Mission = ({ mission }) => {
-  const {
-    missionname, description,
-  } = mission;
-
-  Mission.propTypes = {
-    mission: PropTypes.node.isRequired,
-    missionname: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-  };
-
-  // const dispatch = useDispatch();
->>>>>>> 2c3f5b9c5169c801a4dfbaccb02630e216c1e7eb
 
   return (
     <tr>
@@ -52,7 +33,6 @@ const Mission = ({ mission }) => {
       <td>{description}</td>
       <td name="status">
         <div className="btn-link">
-<<<<<<< HEAD
           {reserved ? (
             <span
               type="button"
@@ -68,27 +48,10 @@ const Mission = ({ mission }) => {
               Not a member
             </span>
           )}
-=======
-          <span
-            type="button"
-            className="inactive-btn-mission"
-          >
-            Not a member
-          </span>
-        </div>
-        <div className="btn-link">
-          <span
-            type="button"
-            className="active-btn-mission"
-          >
-            Active member
-          </span>
->>>>>>> 2c3f5b9c5169c801a4dfbaccb02630e216c1e7eb
         </div>
       </td>
       <td>
         <div className="btn-link">
-<<<<<<< HEAD
           {reserved ? (
             <button
               type="button"
@@ -106,22 +69,6 @@ const Mission = ({ mission }) => {
               Join Mission
             </button>
           )}
-=======
-          <button
-            type="button"
-            className="reserve-btn-mission"
-          >
-            Join Mission
-          </button>
-        </div>
-        <div className="btn-link">
-          <button
-            type="button"
-            className="leave-btn-mission"
-          >
-            Leave Mission
-          </button>
->>>>>>> 2c3f5b9c5169c801a4dfbaccb02630e216c1e7eb
         </div>
       </td>
     </tr>
